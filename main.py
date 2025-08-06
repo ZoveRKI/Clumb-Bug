1293cc4d2974bff30a07afc777906e0ce51e5bec
ZoveRKI <shiyu5645@gmail.com>
2025-08-06 19:09:28 +0900

    fix: use arrow to select for main version2

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 import subprocess
+import questionary
 
 def main():
     options = {
-        "ldks": "input_get_ldks.py",
-        "drxsw": "input_get_drxsw.py",
+        "🌟ldks": "input_get_ldks.py",
+        "🌟drxsw": "input_get_drxsw.py",
     }
 
-    print("可选模块：")
-    for key in options:
-        print(f"🌟{key}")
+    your_choice = questionary.select(
+        "Please select a module to run:",
+        choices=[
+            f"{key}" for key in options
+        ],
+        # default="🌟drxsw",        # 默认首选项
+        qmark="🌈",
+        pointer="👉",
+        # use_shortcuts=True,       # 启用键盘快捷键
+        # selected_symbol="✔"       # 选中项的符号(多选时使用)
+    ).ask()
 
-    yourchoice = input("请输入要运行的模块关键字：").strip().lower()
-
-    if yourchoice in options:
-        print(f"🔄 正在运行 {options[yourchoice]}...\n")
-        subprocess.run(["python", options[yourchoice]])
+    if your_choice:
+        print(f"🔄 Running {options[your_choice]}...\n")
+        subprocess.run(["python", options[your_choice]])
     else:
-        print("❌ 无效输入，请重新运行")
+        print("❌ No selection, exiting program.")
 
 if __name__ == "__main__":
     main()
